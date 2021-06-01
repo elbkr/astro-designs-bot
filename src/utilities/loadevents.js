a49899dfddf4a4fe3d94003734385561d45801fc
elttayman <thettayman@gmail.com>
2021-06-01 13:11:10 +0200

    Update loadevents.js

--- src/utilities/loadevents.js
+++ src/utilities/loadevents.js
@@ -1,1 +1,15 @@
+const reqEvent = (event) => require(`../events/${event}`);
+const Discord = require("discord.js");
 
+module.exports = (client) => {
+  const cooldowns = new Discord.Collection();
+
+  client.on("ready", () => reqEvent("ready")(client));
+  client.on("message", (m) => reqEvent("message")(m, cooldowns));
+  client.on("message", reqEvent("afk"));
+  
+  // warnings and errors
+  client.on("warn", (info) => console.log(info));
+  client.on("error", console.error);
+  client.on("unhandledRejection", console.error);
+};
