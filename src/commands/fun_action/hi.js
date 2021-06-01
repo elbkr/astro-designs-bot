fd14ac085b7b6369bd3c99be987ca6e638ca8184
elttayman <thettayman@gmail.com>
2021-06-01 12:29:18 +0200

    Delete hi.js

--- src/commands/fun_action/hi.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "hi",
-  description: "Say hi",
-  async execute(client, message, args) {
-    message.channel.send("Whats up bruhhh <a:sh_laser:799392390317735956>");
-  },
-};
