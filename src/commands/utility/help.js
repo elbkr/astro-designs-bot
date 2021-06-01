fccf2f4b6b974e2bb258bcbf72d74ba733ee14a9
elttayman <thettayman@gmail.com>
2021-06-01 13:44:46 +0200

    Update help.js

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 const help = require("../../json files/helpMsgs.json");
-const setup = require("../../models/setup");
+const { images_emoji, expressions_emoji, fun_emoji, useful_emoji } = require("../../config.json")
 
 module.exports = {
   name: "help",
@@ -12,69 +12,38 @@ module.exports = {
         "Please make sure I have permission to `ADD_REACTIONS` and `EMBED_LINKS`"
       );
     }
-    const data = await setup
-      .findOne({ Guild: message.guild.id })
-      .catch((err) => console.log(err));
-
+  
     let pr;
-    if (data) {
-      pr = data.Prefix;
-    } else {
-      pr = 'y!';
-    }
+   
+    pr = "s/"
 
     await message.delete();
     
     if (!args[0]) {
       let embed = new MessageEmbed()
-        .setTitle("THB8 commands")
-        .setColor("#ffe65d")
+        .setTitle("ISRP 2 commands")
+        .setColor("#FF2C4B")
         .setDescription("React with the emoji of your choice")
         .addFields(
           {
-            name: "<:sh_mods:813491189780971542> Moderation commands",
+            name: `${images_emoji} Images commands`,
             value: `\u200b`,
             inline: true,
           },
           {
-            name: "<:sh_np:812824625720590367> Music commands",
+            name: `${expressions_emoji} Expression commands`,
             value: `\u200b`,
             inline: true,
           },
           {
-            name: "<:sh_utils:813491190069461082> Useful commands",
-            value: `\u200b`,
-            inline: true,
-          },
-          {
-            name: "<:sh_images:813491189772058627> Images commands",
+            name: `${useful_emoji} Useful commands`,
             value: `\u200b`,
             inline: true,
           }
         )
         .addFields(
           {
-            name: "<:sh_fun:813491190145482773> Fun & actions commands",
-            value: `\u200b`,
-            inline: true,
-          },
-          {
-            name: "<:sh_star:814934724060774490>  Leveling commands",
-            value: `\u200b`,
-            inline: true,
-          },
-          {
-            name: "<:sh_config:813491189705736333> Config commands",
-            value: `\u200b`,
-            inline: true,
-          },
-          {
-            name: "<:sh_18:818196966067667014> NSFW commands",
-            value: `\u200b`,
-            inline: true,
-          },
-          {
-            name: "<:sh_yhf8:820734376714174494> Other commands",
+            name: `${fun_emoji} Fun & actions commands`,
             value: `\u200b`,
             inline: true,
           }
@@ -85,12 +54,7 @@ module.exports = {
       await helpMessage.react("<:sh_mods:813491189780971542>");
       await helpMessage.react("<:sh_np:812824625720590367>");
       await helpMessage.react("<:sh_utils:813491190069461082>");
-      await helpMessage.react("<:sh_images:813491189772058627>");
-      await helpMessage.react("<:sh_fun:813491190145482773>");
-      await helpMessage.react("<:sh_star:814934724060774490>");
-      await helpMessage.react("<:sh_config:813491189705736333>");
-      await helpMessage.react("<:sh_18:818196966067667014>");
-      await helpMessage.react("<:sh_yhf8:820734376714174494>");
+      await helpMessage.react(fun_emoji);
 
       const filter = (reaction, user) =>
         user.id !== message.client.user.id && user.id == message.author.id;
@@ -102,7 +66,7 @@ module.exports = {
         const member = message.guild.member(user);
 
         switch (reaction.emoji.name) {
-          case "sh_mods":
+          case "is_images":
             reaction.users.remove(member).catch(console.error);
 
             let modsMessage = new MessageEmbed()
@@ -120,7 +84,7 @@ module.exports = {
 
             break;
 
-          case "sh_np":
+          case "is_expressions":
             reaction.users.remove(member).catch(console.error);
             let musicMessage = new MessageEmbed()
               .setColor("#ffe65d")
@@ -139,7 +103,7 @@ module.exports = {
             await helpMessage.edit(musicMessage);
 
             break;
-          case "sh_fun":
+          case "is_fun":
             reaction.users.remove(member).catch(console.error);
             let funMessage = new MessageEmbed()
               .setColor("#ffe65d")
@@ -157,7 +121,7 @@ module.exports = {
 
             break;
 
-          case "sh_images":
+          case "is_useful":
             reaction.users.remove(member).catch(console.error);
             let imageMessage = new MessageEmbed()
               .setColor("#ffe65d")
@@ -173,112 +137,7 @@ module.exports = {
             await helpMessage.edit(imageMessage);
 
             break;
-
-          case "sh_utils":
-            reaction.users.remove(member).catch(console.error);
-            let usefulMessage = new MessageEmbed()
-              .setColor("#ffe65d")
-              .setTitle("<:sh_utils:813491190069461082> Useful commands")
-              .setDescription(
-                "Useful commands are used to get info about someone, translating something..."
-              )
-              .addField("available commands", help.helpMsg5)
-              .addField(
-                `\u200b`,
-                `Type \`${pr}help <command>\` to get detailed info about that command`
-              );
-            await helpMessage.edit(usefulMessage);
-
-            break;
-
-          case "sh_star":
-            reaction.users.remove(member).catch(console.error);
-            let levelsMessage = new MessageEmbed()
-              .setColor("#ffe65d")
-              .setTitle("<:sh_star:814934724060774490>  Leveling commands")
-              .setDescription(
-                `Leveling commands are used to check your xp, see the server's xp leaderboard...`
-              )
-              .addField("available commands", help.helpMsg6)
-              .addField(
-                `\u200b`,
-                `Type \`${pr}help <command>\` to get detailed info about that command`
-              );
-            await helpMessage.edit(levelsMessage);
-
-            break;
-
-          case "sh_config":
-            reaction.users.remove(member).catch(console.error);
-            let configMessage = new MessageEmbed()
-              .setColor("#ffe65d")
-              .setTitle("<:sh_config:813491189705736333> Config commands")
-              .setDescription(
-                "Config commands are used to garant a better experience of the bot in the server!"
-              )
-              .addField("available commands", help.helpMsg7)
-              .addField(
-                `\u200b`,
-                `Type \`${pr}help <command>\` to get detailed info about that command`
-              );
-            await helpMessage.edit(configMessage);
-
-            break;
-
-          case "sh_18":
-            reaction.users.remove(member).catch(console.error);
-            if (message.channel.nsfw) {
-              let nsfwMessage = new MessageEmbed()
-                .setColor("#ffe65d")
-                .setTitle("<:sh_18:818196966067667014> NSFW commands")
-                .setDescription(
-                  "NSFW commands can only be used on NSFW marked channels"
-                )
-                .addField(
-                  "Available commands",
-                  `NSFW\r${help.helpMsg9}\rActions\r${help.helpMsg91}`
-                )
-                .addField(
-                  `\u200b`,
-                  `Type \`${pr}help <command>\` to get detailed info about that command`
-                );
-              await helpMessage.edit(nsfwMessage);
-            } else {
-              let nsfwMessage = new MessageEmbed()
-                .setColor("#ffe65d")
-                .setTitle("<:sh_18:818196966067667014> NSFW commands")
-                .setDescription(
-                  "NSFW commands can only be used on NSFW marked channels\rThis commands will be revamped soon..."
-                )
-                .addField(
-                  "available commands",
-                  `To see all available NSFW commands, run \`${pr}help\` on a NSFW marked channel`
-                )
-                .addField(
-                  `\u200b`,
-                  `Type \`${pr}help <command>\` to get detailed info about that command`
-                );
-              await helpMessage.edit(nsfwMessage);
-            }
-            break;
-
-          case "sh_yhf8":
-            reaction.users.remove(member).catch(console.error);
-            let otherMessage = new MessageEmbed()
-              .setColor("#ffe65d")
-              .setTitle("<:sh_yhf8:820734376714174494> Other commands")
-              .setDescription(
-                "Other commands are used to get info about the bot,inviting it to other server..."
-              )
-              .addField("available commands", help.helpMsg8)
-              .addField(
-                `\u200b`,
-                `Type \`${pr}help <command>\` to get detailed info about that command`
-              );
-            await helpMessage.edit(otherMessage);
-
-            break;
-
+            
           default:
             reaction.users.remove(member).catch(console.error);
             break;
@@ -304,7 +163,7 @@ module.exports = {
 
       const embed = new MessageEmbed()
         .setTitle(command.name)
-        .setColor("#FFE65D")
+        .setColor("#FF2C4B")
         .setDescription(command.description);
 
       if(command.aliases) {
