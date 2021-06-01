7c07d65310e483dbdc2c13014c97e7d0f1558a7c
elttayman <thettayman@gmail.com>
2021-06-01 13:10:01 +0200

    Update loadcmds.js

--- src/utilities/loadcmds.js
+++ src/utilities/loadcmds.js
@@ -1,1 +1,19 @@
 
+function loadCommands(client) {
+  const fs = require("fs");
+
+  const commandFolders = fs.readdirSync("./src/commands");
+  for (const folder of commandFolders) {
+    const commandFiles = fs
+      .readdirSync(`./src/commands/${folder}`)
+      .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+      const command = require(`../commands/${folder}/${file}`);
+      client.commands.set(command.name, command);
+    }
+  }
+}
+
+module.exports = {
+  loadCommands,
+};
