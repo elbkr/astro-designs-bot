2065481fa43c2cd6475f38994d99b95ca3e978b8
elttayman <thettayman@gmail.com>
2021-06-01 12:58:16 +0200

    Update ohno.js

--- src/commands/images/ohno.js
+++ src/commands/images/ohno.js
@@ -8,7 +8,7 @@ module.exports = {
   async execute(client, message, args) {
     if (!args) {
       return message.channel.send(
-        "<:sh_wait:817144214667132949> You have to enter a stupid message"
+        "You have to enter a stupid message"
       );
     }
     let text = args.join(" ");
