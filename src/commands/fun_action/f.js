000a94014232fe505c08102084d8b1debbf87770
elttayman <thettayman@gmail.com>
2021-06-01 12:43:12 +0200

    updated emoji

--- src/commands/fun_action/f.js
+++ src/commands/fun_action/f.js
@@ -1,3 +1,5 @@
+const { f_emoji } = require('../../config.json')
+
 module.exports = {
   name: "f",
   description: "pay respects",
@@ -5,11 +7,11 @@ module.exports = {
     let mention = message.mentions.members.first();
     if (!mention)
       return message.channel.send(
-        `${message.author.username} has paid their respect <:sh_f:799392397389856788> `
+        `${message.author.username} has paid their respect ${f_emoji}`
       );
     const member = message.mentions.members.first();
     const mentioned = message.mentions.members.first();
-    if (mentioned.id === "800074066949832714")
+    if (mentioned.id === "849233562016546826")
       return message.channel.send(
         `${message.author.username} has paid their respect for.... me!? Don't kick me please :(`
       );
@@ -17,7 +19,7 @@ module.exports = {
       return message.channel.send(
         `${message.author.username} has paid their respect for **${args.join(
           " "
-        )}** <:sh_f:799392397389856788> `
+        )}** ${f_emoji}`
       );
   },
 };
