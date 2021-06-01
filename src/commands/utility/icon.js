b1fba773081360936705c14cf5f359040f1d479f
elttayman <thettayman@gmail.com>
2021-06-01 13:15:58 +0200

    Create icon.js

---
+++ src/commands/utility/icon.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "icon",
+  description: "Sends the current server's icon.",
+  aliases: ["server-icon", "guild-icon"],
+  async execute(client, message, args) {
+    if (!message.guild.available)
+      return this.client.logger.info(
+        `Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`
+      );
+
+    const embed = new MessageEmbed()
+      .setColor("#ffe65d")
+      .setTitle(`Icon of ${message.guild.name}`)
+      .setImage(
+        `${message.guild.iconURL({ size: 1024, dynamic: true, format: "png" })}`
+      );
+    message.channel.send({ embed });
+  },
+};
